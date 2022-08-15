f76b574f1eec05afb0bb40f4f93dbaa290fab558
010278Francisco <96147436+010278Francisco@users.noreply.github.com>
2022-08-15 14:42:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "francisco012"
+  name  = "env.DOCKER_REPO"
   ports {
     internal = 80
-    external = 83
+    external = CONTAINER_PORT
   }
 }
