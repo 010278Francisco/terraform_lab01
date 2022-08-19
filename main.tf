2db6c85a2071dcff366aec19a9c758f49067accc
010278Francisco <96147436+010278Francisco@users.noreply.github.com>
2022-08-18 20:39:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,9 +58,38 @@ resource "aws_eip" "myFirstInstance"{
 	Name = "my_elastic_ip"
 
 	}
-
+}
+#//////////////////////////////////////////////////////////
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
 }
 
+
+variable "reponame" {}
+variable "container_port" {}
+
+
+provider "docker" {}
+
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = var.reponame #Se manda llamar las variable nombre directo de nuestro jenkingsPipeline
+  ports {
+    internal = 80
+    external = var.container_port  #Se manda llamar las variable del puerto directo de nuestro jenkingsPipeline
+  }
+}
 
 
 
