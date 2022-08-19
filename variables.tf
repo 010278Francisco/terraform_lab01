120d24983061b8a672c551dc0b27d77cb2de0461
010278Francisco <96147436+010278Francisco@users.noreply.github.com>
2022-08-18 19:47:28 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+
+variable "aws-region"{
+description = " The AWS region to create things"
+default     = "us-east-2"
+}
+
+variable "key_name"{
+  description = "SSH keys to connect to ec2 instance"
+  default     = "ssh-v1"
+}
+
+variable "instance_type"{
+  description = "instance type for ec2"
+  default     = "t2.micro"
+}
+
+variable "security_group"{
+  description = "Name of the security-group"
+  default     = "my-jenkins"
+}
+
+variable "tag_name"{
+  description = "Tag Name of for Ec2 instance"
+  default     = "server"
+}
+
+variable "ami_id"{
+  description = "Ami for ubuntu ec2 instance"
+  default     = "ami-002068ed284fb165b"
+}
