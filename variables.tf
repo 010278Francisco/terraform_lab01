7581af8370a12f5400ec1831cee9701f9f8324cf
010278Francisco <96147436+010278Francisco@users.noreply.github.com>
2022-08-18 20:32:39 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 
 variable "aws_region"{
-description = " The AWS region to create things"
-default     = "us-east-2"
+  description = " The AWS region to create things"
+  default     = "us-east-2"
 }
 
 variable "key_name"{
