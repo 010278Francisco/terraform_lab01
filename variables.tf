269908d39cc98c3dbbeeffa725c2bc9ee96fe342
010278Francisco <96147436+010278Francisco@users.noreply.github.com>
2022-08-19 00:28:46 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "key_name"{
 
 variable "instance_type"{
   description = "instance type for ec2"
-  default     = "t2.micro01"
+  default     = "t2.micro"
 }
 
 variable "security_group"{
