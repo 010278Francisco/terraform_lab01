4a87f86adb25b064065566d8c2298e488db0fb1b
010278Francisco <96147436+010278Francisco@users.noreply.github.com>
2022-08-18 23:53:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,41 @@
 
 #//////////////////////////////////////////////////////////
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+
+
+variable "reponame" {}
+variable "container_port" {}
+
+
+provider "docker" {}
+
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = var.reponame #Se manda llamar las variable nombre directo de nuestro jenkingsPipeline
+  ports {
+    internal = 80
+    external = var.container_port  #Se manda llamar las variable del puerto directo de nuestro jenkingsPipeline
+  }
+}
+
+
+
+
+#//////////////////////////////////////////////////////////
 provider "aws" {
   region  = var.aws_region
   access_key = var.acces_key
@@ -63,38 +99,7 @@ resource "aws_eip" "myFirstInstance"{
 	}
 }
 
-#//////////////////////////////////////////////////////////
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "~> 2.15.0"
-    }
-  }
-}
 
-
-
-variable "reponame" {}
-variable "container_port" {}
-
-
-provider "docker" {}
-
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = true
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = var.reponame #Se manda llamar las variable nombre directo de nuestro jenkingsPipeline
-  ports {
-    internal = 80
-    external = var.container_port  #Se manda llamar las variable del puerto directo de nuestro jenkingsPipeline
-  }
-}
 
 
 
