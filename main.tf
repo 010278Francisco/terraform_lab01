8b68a95d9a19babd3ab098d219728f62a4054d8a
010278Francisco <96147436+010278Francisco@users.noreply.github.com>
2022-08-18 20:53:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,13 @@
 #//////////////////////////////////////////////////////////
 terraform {
   required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "~> 2.15.0"
-    }
-     aws = {
+    aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 
